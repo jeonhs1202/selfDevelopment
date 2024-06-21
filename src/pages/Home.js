f80df9889b5c516f5a285a88485ddb0b60a87aa1
Sunstar <jeonhs1202@gmail.com>
2024-06-21 17:44:16 +0900

    파일 위치 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import testImg from '../asset/img/test.jpg';
 
-const Home = () => {
+export const Home = () => {
   return (
     <div>
       <h1>홈페이지</h1>
       <p>라우터 실습을 위하여 제작</p>
+      <img width={100} height={200} src={testImg} alt='testImg' />
     </div>
   );
 };
